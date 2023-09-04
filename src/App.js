b5f1b8d693947871f7b9252ed7887e5ded3e12bf
EAM26 <emuller2607@gmail.com>
2023-09-04 09:59:15 +0200

    Add standard pages and routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from "react";
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import Navigation from "./components/navigation/Navigation";
+import Login from "./pages/login/Login";
+import MainMenu from "./pages/mainMenu/MainMenu";
+import NotFound from "./pages/notFound/NotFound";
 
 function App() {
-  return (
-    <><h1>Test App</h1></>
-  );
+    return (
+        <>
+            <Navigation/>
+            <Routes>
+                <Route path="/" element={<Login/>}/>
+                <Route path="/main-menu" element={<MainMenu/>}/>
+                <Route path="*" element={<NotFound/>}/>
+            </Routes>
+        </>
+    );
 }
 
 export default App;
