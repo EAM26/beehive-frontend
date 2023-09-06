75fe8df1c548404d2ef7aa9a558ac8e028a10844
EAM26 <emuller2607@gmail.com>
2023-09-06 17:18:34 +0200

    Add styling to weekpage

--- src/components/weekpage/WeekPage.js
+++ src/components/weekpage/WeekPage.js
@@ -2,33 +2,39 @@ import React from 'react';
 import './WeekPage.module.css'
 import styles from './WeekPage.module.css'
 import Button from "../button/Button";
-import caret_left from '../../assets/caret-left.svg'
-import caret_right from '../../assets/caret-right.svg'
-import calendar from '../../assets/calendar.svg'
-
+import {CalendarBlank, CaretLeft, CaretRight} from "@phosphor-icons/react";
 
 
 function WeekPage() {
     return (
         <div className={`outer-container ${styles["week-outer"]}`}>
-            <div className={"inner-container"}>
-                <section className={styles["options-bar"]}>
-                    <p>Week 3</p>
-                    <div className={styles["icon-box"]}>
-                        <Button><img src={caret_left} alt="icon"/></Button>
-                        <Button><img src={calendar} alt="icon"/></Button>
-                        <Button><img src={caret_right} alt="icon"/></Button>
+            <div className={`inner-container ${styles["week-inner"]}`}>
+                <section className={styles["options-bar-general"]}>
+                    <div className={styles["options-bar-left"]}>
+                        <p>Week 3</p>
+                        <div className={styles["icon-box"]}>
+                            <Button className={`${styles["car-left"]} ${styles["no_border_button"]}`}><CaretLeft
+                                size={20}/></Button>
+                            <Button className={`${styles["no_border_button"]}`}><CalendarBlank size={20}/></Button>
+                            <Button className={`${styles["car-right"]} ${styles["no_border_button"]}`}><CaretRight
+                                size={20}/></Button>
+                        </div>
+                        <Button text="VANDAAG"/>
                     </div>
-                    <Button text="VANDAAG"/>
-                    <Button text="DAG"/>
-                    <Button text="WEEK"/>
-                    <Button text="PUBLICEREN"/>
-
+                    <div className={styles["options-bar-right"]}>
+                        <div className={styles.test}>
+                            <Button className={`${styles["dag-but"]} ${styles["no_border_button"]}`} text="DAG"/>
+                            <Button className={`${styles["week-but"]} ${styles["no_border_button"]}`} text="WEEK"/>
+                        </div>
+                        <Button className={styles.test} text="PUBLICEREN"/>
+                    </div>
 
                 </section>
-                <section></section>
-                <section></section>
-                <section></section>
+                {/*<section>*/}
+                {/*    /!*<div>BEDIENING</div>*!/*/}
+                {/*</section>*/}
+                {/*<section></section>*/}
+                {/*<section></section>*/}
             </div>
         </div>
     );
