69c75ab9d9eb33f270e3506257a6fb734947db90
EAM26 <emuller2607@gmail.com>
2023-09-14 14:48:15 +0200

    Add styling in weekpage components

--- src/components/week-row/WeekRow.js
+++ src/components/week-row/WeekRow.js
@@ -6,13 +6,13 @@ function WeekRow() {
     return (
         <div>
             <ul className={styles['week-row']}>
-            <DayOfWeek name={'ma'} date={'1/1/2021'}/>
-            <DayOfWeek name={'di'} date={'1/1/2021'}/>
-            <DayOfWeek name={'wo'} date={'1/1/2021'}/>
-            <DayOfWeek name={'do'} date={'1/1/2021'}/>
-            <DayOfWeek name={'vr'} date={'1/1/2021'}/>
-            <DayOfWeek name={'za'} date={'1/1/2021'}/>
-            <DayOfWeek name={'zo'} date={'1/1/2021'}/>
+            <DayOfWeek name={'ma'} date={'7/1/2021'}/>
+            <DayOfWeek name={'di'} date={'8/1/2021'}/>
+            <DayOfWeek name={'wo'} date={'9/1/2021'}/>
+            <DayOfWeek name={'do'} date={'10/1/2021'}/>
+            <DayOfWeek name={'vr'} date={'11/1/2021'}/>
+            <DayOfWeek name={'za'} date={'12/1/2021'}/>
+            <DayOfWeek name={'zo'} date={'13/1/2021'}/>
 
             </ul>
         </div>
