02e2ba834648833f89a7d72d95c05d186bede932
EAM26 <emuller2607@gmail.com>
2023-09-20 12:53:57 +0200

    Add login page

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import styles from './Login.module.css';
+import Button from "../../components/button/Button";
+// import {useForm} from "react-hook-form";
 
 function Login() {
+    function onFormSubmit(event) {
+        event.preventDefault();
+    }
+
     return (
-        <div>Login</div>
+        <div className='outer-container'>
+            <div className='inner-container'>
+
+                    <form className={styles["week-outer"]} onSubmit={onFormSubmit}>
+                        <fieldset className={styles["week-outer"]}>
+                            <legend>Login</legend>
+                            <label htmlFor="user-name">
+                                <input type="text" id="user-name" name="name" placeholder="   Gebruikersnaam"/>
+                            </label>
+                            <label htmlFor="password">
+                                <input type="password" id="password" name="password" placeholder="   Wachtwoord"/>
+                            </label>
+                            <p><a className={styles['login-a']} href="/week">Moeite met inloggen?</a></p>
+                            <Button className={styles['login-button']} type="submit" text="Log in"/>
+                        </fieldset>
+                    </form>
+
+            </div>
+        </div>
     );
 }
 
