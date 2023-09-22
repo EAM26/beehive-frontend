8f73e00e0bf1bb8f048fc84d259b68f5a2c5d01a
EAM26 <emuller2607@gmail.com>
2023-09-22 13:12:39 +0200

    Add Styling Login page

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styles from './Login.module.css';
 import Button from "../../components/button/Button";
+import originalBee from '../../assets/original-bee.svg';
+
 // import {useForm} from "react-hook-form";
 
 function Login() {
@@ -11,19 +13,20 @@ function Login() {
     return (
         <div className='outer-container'>
             <div className='inner-container'>
-
                     <form className={styles["week-outer"]} onSubmit={onFormSubmit}>
-                        <fieldset className={styles["week-outer"]}>
-                            <legend>Login</legend>
+                            <div className={styles['head-login']}>
+                                <img src={originalBee} alt="original-bee"/>
+                                <h2 className={styles['header-text']}>Bee Login</h2>
+                            </div>
+                        <div className={styles['form-field']}><p>Voer hieronder je gebruikersnaam en wachtwoord in om in te loggen</p>
                             <label htmlFor="user-name">
-                                <input type="text" id="user-name" name="name" placeholder="   Gebruikersnaam"/>
+                                <input className={styles['input-field']} type="text" id="user-name" name="name" placeholder="Gebruikersnaam"/>
                             </label>
                             <label htmlFor="password">
-                                <input type="password" id="password" name="password" placeholder="   Wachtwoord"/>
+                                <input className={styles['input-field']} type="password" id="password" name="password" placeholder="Wachtwoord"/>
                             </label>
-                            <p><a className={styles['login-a']} href="/week">Moeite met inloggen?</a></p>
-                            <Button className={styles['login-button']} type="submit" text="Log in"/>
-                        </fieldset>
+                            <a className={styles['login-a']} href="/week">Moeite met inloggen?</a>
+                            <Button className={styles['login-button']} type="submit" text="Log in"/></div>
                     </form>
 
             </div>
