2d3887e56a86a0cf48e98d8835f8dcb43e35c983
EAM26 <emuller2607@gmail.com>
2023-11-07 14:36:43 +0100

    Adapted version of TempGetReq

--- src/pages/Temp/TempGetReq.js
+++ src/pages/Temp/TempGetReq.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import './TempGetReq.css'
 import axios from "axios";
-
+// Aanpassing
 function TempGetReq() {
     const [employees, setEmployees] = useState([])
     useEffect(() => {
